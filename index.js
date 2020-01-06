22735447fcb51af2a8f44aac2ee4564898e223e5
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-01-06 13:55:50 -0500

    Adds the second exercise to have specialized greetings depending on the name

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const app = express();
 
 const server = http.createServer(app);
 
+const VIPS = ['Joe', 'Oakley', 'Matt Damon'];
+
+app.get(`/`, (req, res) => {
+    res.send(`Please enter your name after the / in the URL`);
+})
+
+app.get(`/:name`, (req, res) => {
+    if (VIPS.includes(req.params.name)) {
+        res.send(`How wonderfully splendid it is to be in your presence again, ${req.params.name}! You look magnificent today.`);
+    } else{
+        res.send(`Hello, ${req.params.name}!`);
+    }
+})
+
 server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
 })
